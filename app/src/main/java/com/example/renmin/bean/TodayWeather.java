6ccd4d505337f3d7fef1f890afb6c83d34a0393a
qq123renmin <qq123renmin@163.com>
2015-03-24 21:25:27 +0800

    change

--- app/src/main/java/com/example/renmin/bean/TodayWeather.java
+++ app/src/main/java/com/example/renmin/bean/TodayWeather.java
@@ -4,4 +4,32 @@ package com.example.renmin.bean;
  * Created by renmin on 2015/3/24.
  */
 public class TodayWeather {
+    private String city;
+    private String updatetime;
+    private String wendu;
+    private String shidu;
+    private String pm25;
+    private String quality;
+    private String fengxiang;
+    private String fengli;
+    private String date;
+    private String high;
+    private String low;
+    private String type;
+
+    public String getLow() {
+        return low;
+    }
+
+    public String getHigh() {
+        return high;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
