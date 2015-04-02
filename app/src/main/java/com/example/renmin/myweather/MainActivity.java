d819e33a7291446acdafc0fa84d0e1b9c564ae2d
qq123renmin <qq123renmin@163.com>
2015-04-02 15:34:39 +0800

    change

--- app/src/main/java/com/example/renmin/myweather/MainActivity.java
+++ app/src/main/java/com/example/renmin/myweather/MainActivity.java
@@ -1,17 +1,12 @@
 package com.example.renmin.myweather;
 
-import android.content.DialogInterface;
 import android.content.SharedPreferences;
-import android.nfc.Tag;
+import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,8 +26,7 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringReader;
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
+
 import java.util.zip.GZIPInputStream;
 
 
@@ -229,7 +223,6 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         final String address = "http://wthrcdn.etouch.cn/WeatherApi?citykey=" + cityCode;
         Log.d("myWeather", address);
         new Thread(new Runnable() {
-
             @Override
             public void run() {
                 TodayWeather todayWeather;
